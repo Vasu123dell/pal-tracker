c49e1506185ccee7c18c7abf7271b456b7244239
vasu123dell <vasudev_madasu@dell.com>
2019-09-09 15:23:57 +0530

    push with manifest

--- src/PalTracker/WelcomeController.cs
+++ src/PalTracker/WelcomeController.cs
@@ -5,7 +5,18 @@ namespace PalTracker
     [Route("/")]
     public class WelcomeController : ControllerBase
     {
+         private readonly WelcomeMessage _message;
+
+        //[HttpGet]
+        //public string SayHello() => "hello";
+
         [HttpGet]
-        public string SayHello() => "hello";
+        public string SayHello() => _message.Message;
+        public WelcomeController(WelcomeMessage message)
+        {
+            _message = message;
+        }
     }
+
+    
 }
